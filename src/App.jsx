6f959d9f8f2d392b7185d49c83f0c66035f58b00
oguzhanbayoglu <oguzhanbayoglu1@gmail.com>
2023-05-27 17:51:43 +0300

    tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-black">
+      <p className=" m-4 font-normal hover:font-bold text-white">a</p>
+    </div>
+  );
+}
+
+export default App;
